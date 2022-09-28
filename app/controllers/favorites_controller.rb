48078e837f2f1509df8fc6184c9257c10b0c5025
camoes <cam@camoes.fr>
2022-09-28 20:37:08 +0100

    commit

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,8 @@
 class FavoritesController < ApplicationController
 
+  respond_to :html, :json, :js
   def create
+
     current_user.favorites.create(blog_id: params[:blog_id])
     redirect_to blogs_path
   end
